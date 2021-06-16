6f2ea4e3ad019792295cf3645db75500502fcb6e
Carlos Bronze <170220+bronze@users.noreply.github.com>
2021-06-15 23:49:53 -0300

    rem pages

--- vite.config.ts
+++ vite.config.ts
@@ -53,11 +53,11 @@ const config: UserConfig = {
       extendRoute(route) {
         const path = resolve(__dirname, route.component.slice(1))
 
-        if (!path.includes('projects.md')) {
-          const md = fs.readFileSync(path, 'utf-8')
-          const { data } = matter(md)
-          route.meta = Object.assign(route.meta || {}, { frontmatter: data })
-        }
+        // if (!path.includes('projects.md')) {
+        //   const md = fs.readFileSync(path, 'utf-8')
+        //   const { data } = matter(md)
+        //   route.meta = Object.assign(route.meta || {}, { frontmatter: data })
+        // }
 
         return route
       },
