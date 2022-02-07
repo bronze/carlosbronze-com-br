c8482e8001fbdbd4390a339caf367e4623cdad5b
Carlos Bronze <170220+bronze@users.noreply.github.com>
2022-02-07 13:58:27 -0300

    astro

---
+++ README.md
@@ -0,0 +1,1 @@
+npm i -D astro critical sass tailwindcss @11ty/eleventy-img astro-icon autoprefixer postcss-preset-env
---
+++ critical.js
@@ -0,0 +1,9 @@
+const critical = require('critical');
+critical.generate({
+  inline: true,
+  base: 'dist/',
+  src: 'index.html',
+  target: 'index.html',
+  width: 1300,
+  height: 900,
+});
