37228718e3f513639431de8e97c8c29b6d603544
Carlos Bronze <170220+bronze@users.noreply.github.com>
2022-04-05 19:30:20 -0300

    astro 0.26

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,14 @@
-// Full Astro Configuration API Documentation:
-// https://docs.astro.build/reference/configuration-reference
+import { defineConfig } from 'astro/config';
+import tailwind from '@astrojs/tailwind';
+import sitemap from '@astrojs/sitemap';
 
-// @type-check enabled!
-// VSCode and other TypeScript-enabled text editors will provide auto-completion,
-// helpful tooltips, and warnings if your exported object is invalid.
-// You can disable this by removing "@ts-check" and `@type` comments below.
-
-// @ts-check
-export default /** @type {import('astro').AstroUserConfig} */ ({
-  // Enable the Preact renderer to support Preact JSX components.
-  renderers: [],
-  // files in `public/` will be blindly copied to `public/`
-  public: 'public',
-  // `dist/` is where the built website will be output to
-  dist: 'dist',
-  buildOptions: {
-    sitemap: true,
-    site: 'https://www.carlosbronze.com.br/',
-  },
+// https://astro.build/config
+export default defineConfig({
   vite: {
     ssr: {
-      external: ["svgo", "@11ty/eleventy-img"],
+      external: ["svgo"],
     },
   },
+  integrations: [tailwind(), sitemap()],
+  site: 'https://www.carlosbronze.com.br/',
 });
