4788956cc2fc64e9b494b40b9f189ba92b461bd5
Carlos Bronze <170220+bronze@users.noreply.github.com>
2022-08-16 11:26:15 -0300

    toasty alternatives

--- src/scripts/easteregg.js
+++ src/scripts/easteregg.js
@@ -1,21 +1,7 @@
-// https://kevinpy.github.io/KonamiJS/
-// https://github.com/KevinPy/KonamiJS
-function konami() {
+import toasty from 'toasty';
+import Konami from 'konami';
 
-  var arr = [];
-  var konami = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65, 13];
-
-  document.addEventListener('keyup', function (e) {
-    arr.push(e.keyCode);
-    if (arr.length >= 12) arr.shift();
-    console.log(arr);
-    if (arr.toString() === konami.toString()) cheatMode();
-  });
-
-}
-
-function cheatMode() {
-  document.querySelector('#konami').innerHTML += "KONAMI ";
-}
-
-konami();
+const easterEgg = new Konami(() => {
+  let t = toasty();
+  t.trigger();
+})
