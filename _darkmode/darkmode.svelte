436c793e7a2b8b1c5c548c030c286d6c019d764f
Carlos Bronze <170220+bronze@users.noreply.github.com>
2022-10-29 02:09:40 -0300

    save darkmode.svelte source

--- _darkmode/darkmode.svelte
+++ _darkmode/darkmode.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  // https://stackoverflow.com/questions/73341205/why-does-the-icon-in-this-astro-svelte-component-flicker-on-refresh
   import { onMount } from "svelte";
   let theme = localStorage.getItem('theme') ?? 'light';
   let flag = false;
