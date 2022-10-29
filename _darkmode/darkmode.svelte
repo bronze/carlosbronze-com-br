3454139b0d406b32840bbd6aa06f60053a920b14
Carlos Bronze <170220+bronze@users.noreply.github.com>
2022-10-29 02:09:10 -0300

    save darkmode.svelte

---
+++ _darkmode/darkmode.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+  let theme = localStorage.getItem('theme') ?? 'light';
+  let flag = false;
+  onMount(()=>{
+    flag = true
+  })
+  $: if (flag) {
+    if ( theme === 'dark') {
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+    localStorage.setItem("theme", theme);
+  }
+
+  const handleClick = () => {
+    theme = (theme === "light" ? "dark" : "light");
+  };
+</script>
+
+<button on:click={handleClick}>{theme === "dark" ? "🌕" : "🌑"}</button>
