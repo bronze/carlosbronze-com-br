1c9c2d3ce1427669b24f2db080305b4d7b5e2e27
Carlos Bronze <170220+bronze@users.noreply.github.com>
2023-09-22 21:16:09 -0300

    fluid text

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -30,6 +30,44 @@ module.exports={
         transparent: "transparent",
         current: "currentColor",
       },
+      // typography: ({theme}) => {
+      //   return {
+      //     DEFAULT: {
+      //       css: {
+      //         '--tw-prose-headings': 'var(--color-text-default)', // opacity
+      //         '--tw-prose-body': 'var(--color-text-default)',
+      //         '--tw-prose-links': 'var(--color-text-default)',// opacity
+      //         a: {
+      //           // change anchor color and on hover
+      //           // color: '#03989E',
+      //           // color: 'rgb(var(--color-primary) / 1)',
+      //           // https://stackoverflow.com/questions/72831003/tailwind-custom-theme-color-opacity-not-being-applied
+      //           // https://tailwindcss.com/blog/tailwindcss-v3-1#easier-css-variable-color-configuration
+      //           // color: withOpacity('--color-accent'),
+      //           '&:hover': { // could be any. It's like extending css selector
+      //             // color: '#03989E',
+      //             color: 'rgb(var(--color-primary) / 1)',
+      //             // 'color': 'var(--color-primary)',
+      //             // color: withOpacity('--color-primary'),
+      //             // fill: withOpacity('--color-primary'),
+      //           },
+      //         },
+      //       }
+      //     },
+      //   }
+      // },
+      // fontSize: {
+      //   '2xl': ['1.5rem', {
+      //     lineHeight: '2rem',
+      //     letterSpacing: '-0.01em',
+      //     fontWeight: '500',
+      //   }],
+      //   '3xl': ['1.875rem', {
+      //     lineHeight: '2.25rem',
+      //     letterSpacing: '-0.02em',
+      //     fontWeight: '700',
+      //   }],
+      // },
       fontFamily: {
         sans: ["Inter Variable", "Inter", ...defaultTheme.fontFamily.sans],
         nunito: ["Nunito", ...defaultTheme.fontFamily.sans],
@@ -40,5 +78,42 @@ module.exports={
     require('tailwind-nord'),
     require('@tailwindcss/forms'),
     require('@tailwindcss/typography'),
+    require('tailwindcss-fluid-type')({
+      // your fluid type settings
+      // works only with unitless numbers
+      // This numbers are the defaults settings
+      settings: {
+        fontSizeMin: 1.125, // 1.125rem === 18px
+        fontSizeMax: 1.25, // 1.25rem === 20px
+        ratioMin: 1.125, // Multiplicator Min
+        ratioMax: 1.2, // Multiplicator Max
+        screenMin: 20, // 20rem === 320px
+        screenMax: 96, // 96rem === 1536px
+        unit: 'rem', // default is rem but it's also possible to use 'px'
+        prefix: 'f', // set a prefix to use it alongside the default font sizes
+        extendValues: true, // When you set extendValues to true it will extend the default values. Set it to false to overwrite the values.
+      },
+      // Creates the text-xx classes
+      // This are the default settings and analog to the tailwindcss defaults
+      // Each `lineHeight` is set unitless and we think that's the way to go especially in context with fluid type.
+      values: {
+        'xs': [-2, 1.6],
+        'sm': [-1, 1.6],
+        'base': [0, 1.6],
+        'lg': [1, 1.6],
+        'xl': [2, 1.2],
+        '2xl': [3, 1.2],
+        '3xl': [4, 1.2],
+        '4xl': [5, 1.1],
+        '5xl': [6, 1.1],
+        '6xl': [7, 1.1],
+        '7xl': [8, 1],
+        '8xl': [9, 1],
+        '9xl': [10, 1],
+      },
+    }),
   ],
+  variants: {
+    fluidType: ['responsive']
+  }
 };
