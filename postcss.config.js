23e123d4a5abe60294b32e7383dee16f988e7aff
Carlos Bronze <170220+bronze@users.noreply.github.com>
2023-10-02 21:31:50 -0300

    general improvements

--- postcss.config.js
+++ postcss.config.js
@@ -1,13 +1,12 @@
-const postcssPresetEnv=require('postcss-preset-env');
-
-
 // postcss.config.js
 module.exports={
   plugins: {
     'postcss-import': {},
     'tailwindcss/nesting': {},
     tailwindcss: {},
-    'postcssPresetEnv': {},
+    'postcss-preset-env': {
+      features: {'nesting-rules': false},
+    },
     autoprefixer: {},
   }
 }
