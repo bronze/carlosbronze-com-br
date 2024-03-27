a3d7cea3d72f428b7781984c7fbeda3f58b428eb
Carlos Bronze <170220+bronze@users.noreply.github.com>
2024-03-27 18:37:02 -0300

    vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -27,7 +27,9 @@ export default defineConfig({
     // SVG: true
   })],
   output: "server",
-  adapter: vercel()
+  adapter: vercel({
+    webAnalytics: {enabled: true}
+  }),
 });
 
 // critters(), compressor()
