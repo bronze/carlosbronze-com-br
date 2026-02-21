b3eedd8a270aa9e0b79591fad6dec2efd29bb789
Carlos Bronze <170220+bronze@users.noreply.github.com>
2026-02-21 18:41:02 -0300

    Geist

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,19 @@
 import tailwindcss from "@tailwindcss/vite";
 // @ts-check
-import { defineConfig } from 'astro/config';
+import { defineConfig, fontProviders } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
 	vite: {
 		plugins: [tailwindcss()],
+	},
+	experimental: {
+		fonts: [
+			{
+				provider: fontProviders.fontsource(),
+				name: "Geist",
+				cssVariable: "--font-geist",
+			},
+		],
 	},
 });
